c006b488589ea12d833e6a660dd52048478a3337
DeepAung <i.deepaung@gmail.com>
2026-01-01 14:29:19 +0700

    feat: update INTERPOLATION_DELAY

--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -56,7 +56,7 @@ pub const VIEW_SIZE: Vec2 = Vec2::new(
 
 // ===== Other Constants ===== //
 pub const CLIENT_TIMEOUT: Duration = Duration::from_secs(10);
-pub const INTERPOLATION_DELAY: Duration = Duration::from_millis(100); // Client render delay
+pub const INTERPOLATION_DELAY: Duration = Duration::from_millis(50); // Client render delay
 pub const LAG_COMPENSATION_HISTORY: Duration = Duration::from_secs(5);
 pub const MAX_PACKET_SIZE: usize = 65535;
 
